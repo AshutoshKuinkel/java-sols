e3c5b1fee63c4ac9b72e6636765ae819784eb557
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-03-05 23:14:27 +1100

    .

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     Solution sol = new Solution();
     int[] nums = {2,0,2,1,1,0};
-    sol.sortColors(nums);
+    // sol.sortColors(nums);
   }
 }
